9dbf86ce8fc10f589328e47edd16cee7c3a7f5c0
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-04-06 20:04:32 +0300

    Add Seconds_In_Time project

---
+++ Lesson_2/Seconds_In_Time/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Seconds_In_Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort hours = 24;
+            ushort minutes = 0;
+            ushort seconds = 0;
+            int secresult = (hours * 3600 + minutes * 60 + seconds);
+            Console.WriteLine($"Time {hours}:{minutes}:{seconds} = {secresult} seconds");
+            Console.ReadKey();
+            
+        }
+    }
+}
