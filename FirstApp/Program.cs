9f8051175b71142482d12103338106ea0243371c
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-04-06 17:53:26 +0300

    Add new project

---
+++ FirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Hillel_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Привіт, я навчаюся в школі Hillel");
+            Console.ReadKey();
+        }
+    }
+}
