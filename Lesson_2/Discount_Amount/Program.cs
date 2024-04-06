7e5bce483ed0e80ca05d059682f8fd3ed68d439e
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-04-06 18:46:07 +0300

    Discount_Amount calculation

---
+++ Lesson_2/Discount_Amount/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Discount_Amount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort price = 200;
+            byte discount = 10;
+            decimal sale = price/100*discount;
+
+            if (discount > 100)
+                Console.WriteLine("discount should be less then 100");
+
+            else
+                Console.WriteLine($"You will save {sale} dollars");
+
+
+
+        }
+    }
+}
