1f4921c358feabca124ca1fe8ca201ad268d5dcd
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-04-09 22:41:34 +0300

    Add Numbers_Square

---
+++ Lesson_3/Numbers_Square/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Numbers_Square
+{
+    internal class Program
+    {
+        // Написати програму, яка обчислює квадрат будь-якого введеного числа (бажано використати Math).
+        static void Main(string[] args)
+        {
+            double number;
+            Console.Write("Enter number: ");
+            number = double.Parse(Console.ReadLine());
+            number = Math.Pow(number,2);
+            Console.WriteLine($"Square for you number will be: {number}");
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
