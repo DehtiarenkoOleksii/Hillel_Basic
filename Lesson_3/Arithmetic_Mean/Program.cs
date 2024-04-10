07770c67f769d16e373075041c07194293f13c7f
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-04-10 21:02:36 +0300

    add Arithmetic_Mean

---
+++ Lesson_3/Arithmetic_Mean/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Arithmetic_Mean
+{
+    internal class Program
+    {
+        // Написати програму, яка обчислює середнє арифметичне двох чисел.
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Enter num1: ");
+            string? input1 = Console.ReadLine();
+            Console.Write("Enter num2: ");
+            string? input2 = Console.ReadLine();
+
+            num1 = int.Parse(input1);
+            num2 = int.Parse(input2);
+
+            float average = ((float)num1 + num2) / 2;
+
+            Console.WriteLine($"Average = {average}");
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
