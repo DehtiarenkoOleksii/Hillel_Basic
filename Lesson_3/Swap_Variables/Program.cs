6ef314ae7665cd325a493635a6ea4096abf73a6d
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-04-11 22:47:45 +0300

    Add BitwiseComplement Variant

--- Lesson_3/Swap_Variables/Program.cs
+++ Lesson_3/Swap_Variables/Program.cs
@@ -5,7 +5,7 @@
         //Дослідити інші способи поміняти місцями значення двох змінних без використання тимчасової змінної та використання суми/множення.
         static void Main(string[] args)
         {
-            int num1, num2, num3, num4, num5, num6, num7, num8;
+            int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
             Console.Write("Enter num1: ");
             string? input1 = Console.ReadLine();
             Console.Write("Enter num2: ");
@@ -18,7 +18,8 @@
             num6 = int.Parse(input2);
             num7 = int.Parse(input1);
             num8 = int.Parse(input2);
-
+            num9 = int.Parse(input1);
+            num10 = int.Parse(input2);
 
 
             // Variant_1 Turples
@@ -45,6 +46,12 @@
             Console.WriteLine($"num1 = {num7}");
             Console.WriteLine($"num2 = {num8}");
 
+            // Variant_5 Insidious deception of the user )))
+            BitwiseComplement(ref num9, ref num10);
+            Console.WriteLine("\nVariant_5");                     
+            Console.WriteLine($"num1 = {num9}");
+            Console.WriteLine($"num2 = {num10}");
+
             //Delay
             Console.ReadKey();
         }
@@ -61,6 +68,14 @@
             num8 = num8 ^ num7;
             num7 = num7 ^ num8;
 
+        }
+        static void BitwiseComplement (ref int num9, ref int num10)
+        {
+            //  num9 = 2 in binary  00000000 00000000 00000000 00000010 -> 0..00000010     num10 = 3 in binary 00000000 00000000 00000000 00000011 -> 0..00000011
+
+            num9 = num10 - ~num9 - 1;  // 3 - (0..00000010 (2) -> 1..11111101 (-3)) - 1 == 3 - (-3) - 1 == 3+3-1 == 5
+            num10 = num9 + ~num10 + 1; // 5 + (0..00000011 (3) -> 1..11111100 (-4)) + 1 == 5 + (-4) + 1 == 5-4+1 == 2
+            num9 = num9 + ~num10 + 1;  // 5 + (0..00000010 (2) -> 1..11111101 (-3)) + 1 == 5 + (-3) + 1 == 5-3+1 == 3
         }
     }
 }
