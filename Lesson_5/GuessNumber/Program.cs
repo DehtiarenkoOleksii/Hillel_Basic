30388149b0541736884f8e3ddf9b12fc2cc9e0a8
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-04-16 21:11:14 +0300

    Add GuessNumber

---
+++ Lesson_5/GuessNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace GuessNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            int mysteriousNumber = GenerateRandomNumber(146);
+            CompareNumbers(mysteriousNumber);
+            
+        }
+        private static int GenerateRandomNumber(int limit)
+        {
+            Random rand = new Random();
+            return rand.Next(1, limit);
+        }
+        private static void CompareNumbers(int mysteriousNumber)
+        {
+            int inputNumber;
+            do 
+            {
+                Console.Write("Введіть число : ");
+                inputNumber = Parse(Console.ReadLine());
+                if (inputNumber < mysteriousNumber)
+                {
+                    Console.WriteLine("Введіть більше число!");
+                }
+                else if (inputNumber > mysteriousNumber)
+                {
+                    Console.WriteLine("Введіть менше число!");
+                }
+
+            }
+            while (inputNumber != mysteriousNumber);
+            Console.WriteLine("Ви вгадали!!!");
+        }
+        static int Parse(string input)
+        {
+            return int.Parse(input);
+        }
+    }
+}
