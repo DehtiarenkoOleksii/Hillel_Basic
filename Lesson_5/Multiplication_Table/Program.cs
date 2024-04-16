b99d3c6840298a5b57e6bfb295b7d673b5811660
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-04-16 21:40:44 +0300

    Add Multiplication_Table

---
+++ Lesson_5/Multiplication_Table/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Multiplication_Table
+{
+    internal class Program
+    // Доробити таблицю множення - зробити гарний вивід в консоль.
+    {
+        static void Main(string[] args)
+        {
+            // Таблиця множення
+
+            for (int i = 1; i <= 10; i++) // 1*1=1
+            {
+                Console.WriteLine();
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write($"{j} * {i} = {i * j}\t");
+                }
+            }
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++) // 1*1=1
+            {
+                Console.WriteLine();
+                for (int j = 6; j <= 10; j++)
+                {
+                    Console.Write($"{j} * {i} = {i * j}\t");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
