67554d5681eccc0547fb4130710e11e7a8054008
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-05-06 09:50:41 +0300

    add Books_OOP

---
+++ Lesson_9/Books_OOP/Book.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Books_OOP
+{
+    internal class Book
+    {
+        /// <summary>
+        /// Not required fields in this case if they are commneted, automaticly uses auto-implemeted properties below.
+        /// REMINDER! ask teacher about memory for fields and cases with auto-implemeted properties and validation for setters
+        /// </summary>
+        //private string _title, _author;
+        //private int _year, _pages;
+
+        public string Title { get; set;}
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public int Pages { get; set; }
+
+
+        // Display properties for class instance 
+        public void DisplayInfo()
+        {
+            Console.WriteLine(
+                $"Title: {Title}\n" +
+                $"Author: {Author}\n" +
+                $"Year: {Year}\n" +
+                $"Pages: {Pages}");
+        }
+        // Checks and display info about the thickness of the book
+        public void IsThick() 
+        {
+            Console.WriteLine($"This book is {(Pages > 500 ? "" : "not so")} thick\n");
+        }
+    }
+}
---
+++ Lesson_9/Books_OOP/Program.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Books_OOP
+{
+    internal class Program
+    {
+        #region Simple variant with static data
+        //static void Main(string[] args)
+        //{
+        //    Book book1 = new Book
+        //    {
+        //        Title = "Dune",
+        //        Author = "Frank Herbert",
+        //        Year = 1965,
+        //        Pages = 501,
+        //    };
+        //    Book book2 = new Book
+        //    {
+        //        Title = "1984",
+        //        Author = "George Orwell",
+        //        Year = 1949,
+        //        Pages = 350,
+        //    };
+        //    book1.DisplayInfo();
+        //    book1.IsThick();
+
+        //    book2.DisplayInfo();
+        //    book2.IsThick();
+        //}
+
+        #endregion
+        #region Variant with adding books via objects array with type Book
+        static void Main(string[] args)
+        {
+            Book[] books = GetBooks();
+
+            DisplayAllBooks(books);
+        }
+        // Get book info from user until rejecting continue 
+        static Book[] GetBooks()
+        {
+            // array of Book type objects
+            Book[] books = new Book[0];
+            string answer;
+
+            // loop for adding
+            do
+            {
+                Book book = GetBookInformation();
+                books = SaveBook(books, book);
+
+                Console.WriteLine("Do you want to add one more book? (Y/N): ");
+                answer = Console.ReadLine();
+            }
+            while (answer.ToLower() == "y");
+
+            return books;
+        }
+        // Get info from user about properties and return class instance  
+        static Book GetBookInformation()
+        {
+            Console.WriteLine("Add info about book:");
+
+            Console.Write("Title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Author: ");
+            string author = Console.ReadLine();
+
+            // used for year to, in cause decided that user like books which are wriiten only in our era)))
+            Console.Write("Year: ");
+            int year = ValidateInput("Please enter a valid year greater than 0: ");
+
+            Console.Write("Count of pages: ");
+            int pages = ValidateInput("Please enter a valid number of pages greater than 0: ");
+
+            return new Book
+            {
+                Title = title,
+                Author = author,
+                Year = year,
+                Pages = pages
+            };
+        }
+        // validation for int properties  
+        static int ValidateInput(string errorMessage)
+        {
+            int input;
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out input) || input <= 0)
+                {
+                    ClearLastLine();
+                    Console.Write(errorMessage);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return input;
+        }
+        //Method which clear last line on the Console
+        static void ClearLastLine()
+        {
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.Write(new string(' ', Console.BufferWidth));
+                Console.WriteLine();
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+            }
+        }
+        // add book object to the Book books array
+        public static Book[] SaveBook(Book[] array, Book newBook)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = newBook;
+            return array;
+        }
+        // Print info about all books and about their thicking) 
+        static void DisplayAllBooks(Book[] array)
+        {
+            Console.Clear();
+            Console.WriteLine("Info about your books:");
+            foreach (var book in array)
+            {
+                book.DisplayInfo();
+                book.IsThick();
+            }
+        }
+        #endregion
+    }
+}
