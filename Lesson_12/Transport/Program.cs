02f628dfe89c26d6aa99de179ffe8f7d088663e5
Oleksii Dehtiarenko <oleksii.dehtiarenko@namecheap.com>
2024-05-20 10:39:32 +0300

    Add one more tuoe of object

--- Lesson_12/Transport/Program.cs
+++ Lesson_12/Transport/Program.cs
@@ -11,9 +11,13 @@
             // Object of the base class
             Transport myTransport = new Transport("Bike", 270);
 
+            // Object of a derived class is created by changing the base class
+            Transport MyCar2 = new Car("Opel", 120, 5);
+
             PrintMovement(myCar);
             PrintMovement(myBicycle);
             PrintMovement(myTransport);
+            PrintMovement(MyCar2);
 
             //Delay
             Console.ReadKey();
